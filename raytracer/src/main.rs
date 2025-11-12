e4e68784403d2e1298e2ec68156bc2dda0c0cebc
Nick Thamm <nickthamm@gmx.de>
2025-11-12 16:41:38 +0100

    adjusting rustfmt

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -5,29 +5,29 @@ mod ray;
 use crate::ray::Ray;
 use crate::ray::ray_color;
 
-fn write_color(pixel_color: Color) { // maybe add ostream here
+fn write_color(pixel_color: Color) {
+    // maybe add ostream here
     let rbyte = (255.999 * pixel_color.x()) as u8;
     let gbyte = (255.999 * pixel_color.y()) as u8;
     let bbyte = (255.999 * pixel_color.z()) as u8;
     print!("{rbyte} {gbyte} {bbyte}\n");
 }
 
-
 fn main() {
     // Image setup
     let aspect_ratio: f64 = 16.0 / 9.0;
     let image_width = 400;
     let mut image_height = (image_width as f64 / aspect_ratio) as i32;
-    image_height = if (image_height < 1) {1} else {image_height};
+    image_height = if (image_height < 1) { 1 } else { image_height };
 
     // Camera
     let focal_length = 1.0;
     let viewport_height = 2.0;
-    let viewport_width= viewport_height * (image_width as f64 / image_height as f64);
-    let camera_center = Point3::new(0.0, 0.0, 0.0); 
+    let viewport_width = viewport_height * (image_width as f64 / image_height as f64);
+    let camera_center = Point3::new(0.0, 0.0, 0.0);
 
     // Calculate the vectors across the horizontal and down the vertical viewport edges
-    let viewport_u = Vec3::new(viewport_width,0.0,0.0);
+    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
     let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
 
     // Calculate the horizontal and vertical delta vectors from pixel to pixel.
@@ -35,20 +35,21 @@ fn main() {
     let pixel_delta_v = viewport_v / image_height as f64;
 
     // Calculate the location of the upper left pixel.
-    let viewport_upper_left = camera_center  - Vec3::new(0.0, 0.0, focal_length) - viewport_u/2.0 - viewport_v/2.0;
-    let pixel00_loc = viewport_upper_left + (pixel_delta_u + pixel_delta_v)*0.5;
+    let viewport_upper_left =
+        camera_center - Vec3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+    let pixel00_loc = viewport_upper_left + (pixel_delta_u + pixel_delta_v) * 0.5;
 
     // Render
     print!("P3\n{image_width} {image_height}\n255\n");
     for j in 0..image_height {
-        eprint!("\r Scanlines remaining: {} ", image_height-j);
+        eprint!("\r Scanlines remaining: {} ", image_height - j);
         for i in 0..image_width {
-            let pixel_center = pixel00_loc + (pixel_delta_u * i as f64) + (pixel_delta_v * j as f64);
+            let pixel_center =
+                pixel00_loc + (pixel_delta_u * i as f64) + (pixel_delta_v * j as f64);
             let ray_direction = pixel_center - camera_center;
             let r = Ray::new(camera_center, ray_direction);
             let pixel_color = ray_color(&r);
             write_color(pixel_color);
-                
         }
     }
     eprint!("\rDone.                      \n");
--- raytracer/src/ray.rs
+++ raytracer/src/ray.rs
@@ -1,11 +1,16 @@
-
 use crate::vec3::{Color, Point3, Vec3};
 
 #[derive(Debug, Clone, Copy)]
-pub struct Ray{origin: Point3, direction: Vec3}
+pub struct Ray {
+    origin: Point3,
+    direction: Vec3,
+}
 impl Ray {
     pub fn new(origin: Point3, direction: Vec3) -> Ray {
-        Ray{origin: origin, direction: direction}
+        Ray {
+            origin: origin,
+            direction: direction,
+        }
     }
     pub fn origin(&self) -> Point3 {
         self.origin
@@ -18,19 +23,25 @@ impl Ray {
     }
 }
 pub fn ray_color(r: &Ray) -> Color {
-    if (hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, &r)) {
-        return Color::new(1.0, 0.0, 0.0)
+    let t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r);
+    if (t > 0.0) {
+        let N = (r.at(t) - Vec3::new(0.0, 0.0, -1.0)).unit_vector();
+        return Color::new(N.x() + 1.0, N.y() + 1.0, N.z() + 1.0) * 0.5;
     }
     let unit_direction = r.direction().unit_vector();
-    let a  = (unit_direction.y() + 1.0)*0.5;
-    Color::new(1.0, 1.0,1.0) * (1.0 - a) + Color::new(0.5, 0.7 ,1.0) * a    
+    let a = (unit_direction.y() + 1.0) * 0.5;
+    Color::new(1.0, 1.0, 1.0) * (1.0 - a) + Color::new(0.5, 0.7, 1.0) * a
 }
 
-fn hit_sphere(center: Point3, radius: f64, r:&Ray) ->bool {
+fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> f64 {
     let oc = center - r.origin();
-    let a = r.direction().dot(r.direction());
-    let b = -2.0 * r.direction().dot(oc);
-    let c = oc.dot(oc) - radius * radius;
-    let discriminant = b*b - 4.0*a*c;
-    discriminant >= 0.0
+    let a = r.direction().length_squared();
+    let h = r.direction().dot(oc);
+    let c = oc.length_squared() - radius * radius;
+    let discriminant = h * h - a * c;
+    if (discriminant < 0.0) {
+        return -1.0;
+    } else {
+        return (h - discriminant.sqrt()) / (a);
+    }
 }
