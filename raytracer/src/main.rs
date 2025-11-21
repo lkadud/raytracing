ecb6591a8b721bb508048602ffed0d5fa2feceed
Nick Thamm <nickthamm@gmx.de>
2025-11-21 19:56:21 +0100

    finalizing raytracer and adding image

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -24,6 +24,82 @@ mod common;
 use std::rc::Rc;
 
 fn main() {
+    let mut world = HittableList::new();
+
+    // Adding ground
+    let material_ground = Rc::new(Lambertian::new(Color::new(0.5, 0.5, 0.5)));
+    world.add(Box::new(Sphere::new(
+        Point3::new(0.0, -1000.0, 0.0),
+        1000.0,
+        material_ground,
+    )));
+
+    // Adding small objects
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat = common::random_number();
+            let center = Point3::new(
+                a as f64 + 0.9 * common::random_number(),
+                0.2,
+                b as f64 + 0.9 * common::random_number(),
+            );
+
+            if (center - Point3::new(4.0, 0.2, 0.0)).length() > 0.9 {
+                if (choose_mat < 0.8) {
+                    let albedo = Color::random() * Color::random();
+                    let sphere_material = Rc::new(Lambertian::new(albedo));
+                    world.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
+                } else if (choose_mat < 0.95) {
+                    let albedo = Color::random_interval(0.5, 1.0);
+                    let fuzz = common::random_number_interval(0.0, 0.5);
+                    let sphere_material = Rc::new(Metal::new(albedo, fuzz));
+                    world.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
+                } else {
+                    let n = common::random_number_interval(1.0, 1.5);
+                    let sphere_material = Rc::new(Dielectric::new(n));
+                    world.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
+                };
+            }
+        }
+    }
+
+    // Adding large objects
+    let material1 = Rc::new(Dielectric::new(1.5));
+    world.add(Box::new(Sphere::new(
+        Point3::new(0.0, 1.0, 0.0),
+        1.0,
+        material1,
+    )));
+    let material2 = Rc::new(Lambertian::new(Color::new(0.4, 0.2, 0.1)));
+    world.add(Box::new(Sphere::new(
+        Point3::new(4.0, 1.0, 0.0),
+        1.0,
+        material2,
+    )));
+    let material3 = Rc::new(Metal::new(Color::new(0.7, 0.6, 0.6), 0.0));
+    world.add(Box::new(Sphere::new(
+        Point3::new(-4.0, 1.0, 0.0),
+        1.0,
+        material3,
+    )));
+
+    // Camera
+    let mut cam = Camera::default();
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth = 100;
+
+    cam.vfov = 20.0;
+    cam.lookfrom = Point3::new(-13.0, 2.0, 3.0);
+    cam.lookat = Point3::new(0.0, 0.0, 0.0);
+    cam.vup = Vec3::new(0.0, 1.0, 0.0);
+
+    cam.defocus_angle = 0.6;
+    cam.focus_dist = 10.0;
+    cam.render(&world);
+}
+/*
     // World
     let mut world = HittableList::new();
 
@@ -75,3 +151,4 @@ fn main() {
     cam.focus_dist = 3.0;
     cam.render(&world);
 }
+ */
